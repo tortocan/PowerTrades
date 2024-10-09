f2796b8683557cd158fdbc634817b2fc079677bc
George Tortocan <george.tortocan@gmail.com>
2024-10-09 19:05:10 +0200

    Add retry policy

--- PowerTrades/Reports/ForecastPowerReport.cs
+++ PowerTrades/Reports/ForecastPowerReport.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Polly;
 using PowerService;
 using PowerTrades.Builders;
 using System.Collections.ObjectModel;
@@ -20,62 +21,80 @@ namespace PowerTrades.Reports
 
         public IReadOnlyCollection<PowerTradeCsvModel> Records { get; set; } = [];
 
-        public ForecastPowerReport Generate(DateTime date, TimeZoneInfo timeZoneInfo,string workingDirectory = "./", FileConventionBuilder? fileConventionBuilder = null)
+        public ForecastPowerReport Generate(DateTime date, TimeZoneInfo timeZoneInfo, string workingDirectory = "./", FileConventionBuilder? fileConventionBuilder = null)
         {
-            //date = new DateTime(date.Year, date.Month, date.Day,date.Hour,date.Minute,date.Second,date.Kind);
+            IDisposable? tradeScope = null;
+            using var reportScope = logger.BeginScope($"Report Id: {Guid.NewGuid()}");
             logger.LogInformation("Report started.");
-            IDisposable? loggerScope = null;
+            var isSuccessFull = false;
             try
             {
-                var trades = this.powerService.GetTrades(date, timeZoneInfo);
-                if (trades?.Any() == true)
+                Policy.Handle<Exception>()
+               .Retry(3, (e, i) =>
+               {
+                   logger.LogError(e,$"Retry ({i}) task, An excepion has occured: {e.Message}");
+               })
+               .Execute(() =>
                 {
-                    var results = new Dictionary<int, PowerTradeCsvModel>();
-                    logger.LogInformation($"Report has total trades {trades.Count()}");
-                    foreach (var trade in trades)
+                    var trades = this.powerService.GetTrades(date, timeZoneInfo);
+                    if (trades?.Any() == true)
                     {
-                        loggerScope = logger.BeginScope(trade.TradeId);
-                        var periodGroups = trade.Periods.GroupBy(x => x.Period);
-                        logger.LogDebug($"Working on trade periods found {periodGroups?.Count()}");
-                        foreach (var periodGroup in periodGroups)
+                        var results = new Dictionary<int, PowerTradeCsvModel>();
+                        logger.LogInformation($"Report has total trades {trades.Count()}");
+                        foreach (var trade in trades)
                         {
-                            var volume = periodGroup.Sum(x => x.Volume);
-
-                            if (!results.ContainsKey(periodGroup.Key))
+                            tradeScope = logger.BeginScope($"Trade Id: {trade.TradeId}");
+                            var periodGroups = trade.Periods.GroupBy(x => x.Period);
+                            logger.LogDebug($"Working on trade periods found {periodGroups?.Count()}");
+                            foreach (var periodGroup in periodGroups)
                             {
-                                var periodDate = TimeZoneInfo.ConvertTime(new DateTime(trade.Date.Ticks, trade.Date.Kind), timeZoneInfo);
-                                periodDate = periodDate.AddHours(periodGroup.Key - 1);
+                                var volume = periodGroup.Sum(x => x.Volume);
 
-                                results.Add(periodGroup.Key, new PowerTradeCsvModel() { VolumeDate = periodDate });
+                                if (!results.ContainsKey(periodGroup.Key))
+                                {
+                                    var periodDate = TimeZoneInfo.ConvertTime(new DateTime(trade.Date.Ticks, trade.Date.Kind), timeZoneInfo);
+                                    periodDate = periodDate.AddHours(periodGroup.Key - 1);
+
+                                    results.Add(periodGroup.Key, new PowerTradeCsvModel() { VolumeDate = periodDate });
+                                }
+                                var result = results[periodGroup.Key];
+                                logger.LogDebug($"Sum volume {volume} to current {result.Volume}");
+                                result.Volume += volume;
                             }
-                            var result = results[periodGroup.Key];
-                            logger.LogDebug($"Sum volume {volume} to current {result.Volume}");
-                            result.Volume += volume;
+                            tradeScope?.Dispose();
                         }
-                        loggerScope?.Dispose();
+                        Records = new ReadOnlyCollection<PowerTradeCsvModel>(results.Values?.OrderBy(x => x.VolumeDate).ToList());
+
+                        logger.LogInformation("Start buiding the CSV file.");
+
+                        fileConventionBuilder ??= new FileConventionBuilder(new DateOnly(date.Year, date.Month, date.Day));
+                        var builder = powerTradeCsvBuilder.WithWorkingDirectory(workingDirectory).WithFilename(fileConventionBuilder);
+                        builder.Build(Records);
+                        logger.LogInformation("Finish buiding the CSV file.");
+                        isSuccessFull = true;
                     }
-                    Records = new ReadOnlyCollection<PowerTradeCsvModel>(results.Values?.OrderBy(x => x.VolumeDate).ToList());
-
-                    logger.LogInformation("Start buiding the CSV file.");
-
-                    fileConventionBuilder ??= new FileConventionBuilder(new DateOnly(date.Year, date.Month, date.Day));
-                    var builder = powerTradeCsvBuilder.WithWorkingDirectory(workingDirectory).WithFilename(fileConventionBuilder);
-                    builder.Build(Records);
-                    logger.LogInformation("Finish buiding the CSV file.");
-                }
-                else
-                {
-                    logger.LogWarning("Report has no trades.");
-                }
+                    else
+                    {
+                        logger.LogWarning("Report has no trades.");
+                        throw new ArgumentNullException(nameof(trades));
+                    }
+                });
             }
             catch (Exception ex)
             {
-                logger.LogError(ex, ex.Message);
-                loggerScope?.Dispose();
+                logger.LogCritical(ex, ex.Message);
+                tradeScope?.Dispose();
             }
             finally
             {
-                logger.LogInformation($"Report finish with results {Records.Count}.");
+                if (isSuccessFull)
+                {
+                    logger.LogInformation($"Report finish with results {Records.Count}.");
+                }
+                else
+                {
+                    logger.LogCritical($"Report finish with results {Records.Count}.");
+                }
             }
             return this;
         }
